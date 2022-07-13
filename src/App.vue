1adda336e7c2f7b886951327e442b89ca9cda257
kimchulyeon <guinness987@gmail.com>
2022-07-13 19:22:57 +0900

    연습

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
-<template>hello</template>
+<template>
+	<button @click="add">{{ counter }}</button>
+</template>
 
-<script>
-export default {
-	name: 'App',
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const counter = ref(0)
+
+const add = () => {
+	counter.value++
 }
+
+onMounted(() => {
+	console.log('컴포넌트가 마운트되었습니다::::::::')
+})
 </script>
-
-<style>
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-}
-</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>hello wow</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {}
+	},
+}
+</script>
+
+<style lang="scss" scoped></style>
