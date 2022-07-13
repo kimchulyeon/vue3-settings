d8676f127f2d84ea5b6d72123886acd9667394e8
kimchulyeon <guinness987@gmail.com>
2022-07-13 19:22:33 +0900

    docs: jsconfig설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
 	rules: {
 		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
 		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'vue/multi-word-component-names': 0,
 		'prettier/prettier': [
 			'error',
 			{
@@ -24,4 +25,12 @@ module.exports = {
 			},
 		],
 	},
+	overrides: [
+		{
+			files: ['**/__tests__/*.{j,t}s?(x)', '**/tests/unit/**/*.spec.{j,t}s?(x)'],
+			env: {
+				jest: true,
+			},
+		},
+	],
 }
